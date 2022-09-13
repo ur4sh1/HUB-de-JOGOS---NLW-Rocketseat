6c0dc9990569ba2eb3b8ac32bc705c714db059a7
Lucas <lucas.edgerly@gmail.com>
2022-09-13 15:53:51 -0400

    criando a page do hub de games

---
+++ tailwind.config.cjs
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.tsx','./index.html',
+],
+  theme: {
+    fontFamily:{
+      sans: ['Inter', 'sans-serif']
+    },
+    extend: {
+      colors:{
+        
+      },
+      backgroundImage: {
+        galaxy: "url('/fundo.png')",
+        'nlw-gradiente': 'linear-gradient(89.86deg, #9572FC 0%, #43E7AD 50.52%, #E1D55D 100%)',
+        'game-gradient': 'linear-gradient(180deg, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0.9) 67.08%)',
+      }
+    },
+  },
+  plugins: [
+    
+  ],
+}
